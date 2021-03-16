1ee0006fc107e9c4a74db1461650c26ce533d052
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-03-16 10:22:46 +0900

    chore: edit comment, soon to be implemented

--- Search/Dijkstra.java
+++ Search/Dijkstra.java
@@ -2,7 +2,6 @@
 Learning and implementing a very popular algorithm and 
 one of the best approaches for pathfinding and graph traversals
 
-source: https://www.geeksforgeeks.org/a-search-algorithm/
 @author unobatbayar
 */
 
