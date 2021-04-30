be86a5792ab2770ea5354eaf1083d28f1a706228
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2021-04-30 16:38:03 +0900

    feat: add new maths algorithm/formula

---
+++ Mathematics/Distance.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int x1 = 0;
+    int x2 = 0;
+    int y1 = 4;
+    int y2 = 4;
+
+    // Pythagorean theorem to find distance of (x1, y1) and (x2, y2) coordinates
+    // 𝑑=√(𝑥2−𝑥1)2+(𝑦2−𝑦1)2
+    int distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+    prinft("%d", distance);
+    return 0;
+}
