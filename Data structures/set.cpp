0e064b560627c51f96af1e08e7922fb29718b7c1
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2021-05-11 18:02:53 +0900

    feat: add comments set

--- Data structures/set.cpp
+++ Data structures/set.cpp
@@ -2,14 +2,21 @@
 #include <set>
 using namespace std;
 
+/**
+ * Set does not contain a duplicate element. 
+ * Upon insertion, set automatically sorts it
+ **/
+
 int main()
 {
 	set<int> s1;
-	s1.insert(10);
-	s1.insert(20);
+	s1.insert(30); // s = {30}
+	s1.insert(10); // s = {10, 30}
+	s1.insert(20); // s = {10, 20, 30}
 
 	auto a = s1.find(30);
-	cout << (a != s1.end() ? "found" : "nope") << endl;
-	cout << s1.size(); // Outputs 2
+	cout << (a != s1.end() ? "found" : "nope") << endl; // found
+	cout << s1.size(); // 3
+	
 	return 0;
 }
