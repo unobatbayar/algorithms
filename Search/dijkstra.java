a5090f10d9c52b3e1cdc0e7d09c611eb80edf7e1
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2021-07-02 11:12:21 +0900

    wip

---
+++ Search/dijkstra.java
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+/**
+ * @author unobatbayar
+ * **/
+
+struct edge {
+    int nodes[2];
+    int cost; // add more if you need it
+};
+
+using graph = map<int, vector<edge> >;
+
+
+int main() {
+    
+    // create example graph 
+
+
+    // set start and goal destination
+
+
+    // find and print shortest path
+
+
+    return 0;
+}
