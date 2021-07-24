4e613233c449d2e9da167ffcf020dc4199fa8eb8
unobatbayarls <uno02bt@gmail.com>
2021-07-24 13:38:20 +0900

    refactor: move descript upward

--- Cryptography/rot13.cpp
+++ Cryptography/rot13.cpp
@@ -1,5 +1,12 @@
-/**
+/*
 ROT13 (Rotate by 13 places) Caesar Cipher derivative
 
 @author unobatbayar
-**/
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
--- Recursive/Fibonacci.cpp
+++ Recursive/Fibonacci.cpp
@@ -1,10 +1,11 @@
+/*
+Finds the n-th Fibonacci number using recusion
+
+@author unobatbayar
+*/
 #include <iostream>
 using namespace std;
-/**
-@author unobatbayar
 
-Finds the n-th Fibonacci number using recusion
-**/
 
 int fib(int n){
     cout << n << endl; // to semi-visualize the recusive operations
