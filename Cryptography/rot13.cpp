1753885e68085fa1cf15be30df066b8fe2b91329
unobatbayarls <uno02bt@gmail.com>
2021-07-26 21:31:53 +0900

    feat: add

--- Cryptography/rot13.cpp
+++ Cryptography/rot13.cpp
@@ -1,12 +1,39 @@
 /*
-ROT13 (Rotate by 13 places) Caesar Cipher derivative
+ROT13 algorithm (Rotate by 13 places) / Caesar Cipher derivative
 
 @author unobatbayar
 */
 #include <iostream>
+#include <cctype>
+#include <string>
 using namespace std;
+
+string ROT13(string input)
+{
+
+    string transformed;
+    for (size_t i = 0; i < input.size(); ++i)
+    {
+        if (isalpha(input[i]))
+        {
+            if ((tolower(input[i]) - 'a') < 14)
+                transformed.append(1, input[i] + 13);
+            else
+                transformed.append(1, input[i] - 13);
+        }
+        else
+        {
+            transformed.append(1, input[i]);
+        }
+    }
+    return transformed;
+}
 
 int main()
 {
-    
+    string input;
+    //cout << "Enter plaintext that you would like to use ROT13 on" << endl;
+    //cin >> input;
+    cout << ROT13("hello") << endl;
+    cout << ROT13("uryyb") << endl;
 }
---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,22 @@
+/*
+Finds the n-th Fibonacci number using recusion
+
+@author unobatbayar
+*/
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+int fib(int n)
+{
+    // cout << n << endl; // to semi-visualize the recusive operations
+    if (n < 2)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    cout << fib(4);
+}
