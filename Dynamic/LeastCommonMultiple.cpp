136ddbe041748e9e20efd7a740b89783477eb9d2
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2021-09-30 19:52:46 +0900

    feat: update

---
+++ Dynamic/LeastCommonMultiple.cpp
@@ -0,0 +1,10 @@
+/*
+    Algorithm to find LCM or Least Common Multiple (smallest number which can be divided by both) of two numbers.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
