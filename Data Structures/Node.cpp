bf9fe7e3fa0ebe64a3c8cb3fbfce37b1ddde25b6
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-04-05 18:26:23 +0900

    feat: update Node

--- Data Structures/Node.cpp
+++ Data Structures/Node.cpp
@@ -1,5 +1,22 @@
 class Node {          // Node class
-  public:             // Access specifier
+  private:             // Access specifier
     int value;        // Attribute (int variable)
     Node next;        // Attribute (Node variable)
+  
+  public:
+    // Constructor
+    Node(int value, Node next)
+    {
+        this->value = value;
+        this->next = next;
+    }
+  
+    // Getter
+    int getValue(){
+      return value;
+    }
+   
+    Node getNext(){
+      return next;
+    }
 };
