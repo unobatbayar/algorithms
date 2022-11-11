37137a21c3b1b29d94bbde01b52224d5404aed4a
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2022-11-11 13:35:37 +0900

    add queue datastructure

---
+++ Data Structures/queue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>       // std::cout
+#include <queue>          // std::queue
+using namespace std;
+
+int main () 
+{
+  queue<int> my_queue;
+  
+  my_queue.push(7);
+  my_queue.pop(8);
+}
