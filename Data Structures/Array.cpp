1c6bf26929b821d714fc3445e41a14110d67d226
unobatbayar <usukhbayar.batbayar@music.yamaha.com>
2023-01-17 11:06:09 +0900

    add array data-structure

--- Data Structures/Array.cpp
+++ Data Structures/Array.cpp
@@ -1,1 +1,9 @@
-// todo
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string people[3] = {"Alan Turing", "Nikola Tesla", "Steve Jobs"};
+  cout << people[0];
+  return 0;
+}
