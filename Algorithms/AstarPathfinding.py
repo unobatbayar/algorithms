1cf08d9f7f3518a13577ff2a880b7c3a9e7383d0
Uno <38866249+unobatbayar@users.noreply.github.com>
2023-05-16 14:03:34 +0900

    working version add

--- Algorithms/AstarPathfinding.py
+++ Algorithms/AstarPathfinding.py
@@ -1,33 +1,23 @@
 import heapq
 
-# Function to calculate the Manhattan distance between two points
-def manhattan_distance(p1, p2):
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+# Function to calculate the Manhattan distance between two points (coordinates)
+def manhattan_distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return abs(x1 - x2) + abs(y1 - y2)
 
 # A* search algorithm
-def astar_search(graph, start, goal):
-    # Initialize the open and closed sets
+def astar_search(grid, start, goal):
     open_set = []
-    closed_set = set()
-
-    # Create a dictionary to keep track of the cost to reach each node
-    # Initialize all nodes with a cost of infinity except the start node
-    g_score = {node: float('inf') for node in graph}
-    g_score[start] = 0
-
-    # Create a dictionary to keep track of the estimated total cost for each node
-    f_score = {node: float('inf') for node in graph}
-    f_score[start] = manhattan_distance(start, goal)
-
-    # Add the start node to the open set with its estimated cost
-    heapq.heappush(open_set, (f_score[start], start))
+    heapq.heappush(open_set, (0, start))
+    came_from = {}
+    g_score = {start: 0}
 
     while open_set:
-        # Pop the node with the lowest estimated cost from the open set
-        current = heapq.heappop(open_set)[1]
+        _, current = heapq.heappop(open_set)
 
         if current == goal:
-            # Goal reached, return the path
+            # Goal reached, reconstruct the path
             path = []
             while current in came_from:
                 path.append(current)
@@ -36,47 +26,53 @@ def astar_search(graph, start, goal):
             path.reverse()
             return path
 
-        # Add the current node to the closed set
-        closed_set.add(current)
-
-        for neighbor in graph[current]:
-            if neighbor in closed_set:
-                # Skip if the neighbor is already evaluated
-                continue
-
-            # Calculate the tentative cost to reach the neighbor
-            tentative_g_score = g_score[current] + manhattan_distance(current, neighbor)
-
-            if tentative_g_score < g_score[neighbor]:
-                # This path is better than any previous one, update the scores
-                came_from[neighbor] = current
+        for neighbor in get_neighbors(current, grid):
+            tentative_g_score = g_score[current] + 1
+            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
                 g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = g_score[neighbor] + manhattan_distance(neighbor, goal)
-
-                if neighbor not in open_set:
-                    # Add the neighbor to the open set if it's not already there
-                    heapq.heappush(open_set, (f_score[neighbor], neighbor))
+                f_score = tentative_g_score + manhattan_distance(neighbor, goal)
+                heapq.heappush(open_set, (f_score, neighbor))
+                came_from[neighbor] = current
 
     # Open set is empty and goal was not reached, return an empty path
     return []
 
+# Function to get valid neighbors of a grid cell
+def get_neighbors(cell, grid):
+    row, col = cell
+    rows = len(grid)
+    cols = len(grid[0])
+    neighbors = []
+    if row > 0 and grid[row - 1][col] != '#':
+        neighbors.append((row - 1, col))
+    if row < rows - 1 and grid[row + 1][col] != '#':
+        neighbors.append((row + 1, col))
+    if col > 0 and grid[row][col - 1] != '#':
+        neighbors.append((row, col - 1))
+    if col < cols - 1 and grid[row][col + 1] != '#':
+        neighbors.append((row, col + 1))
+    return neighbors
+
 # Example usage
 
-# Define the graph as an adjacency dictionary
-graph = {
-    'A': {'B', 'C'},
-    'B': {'A', 'D'},
-    'C': {'A', 'D', 'E'},
-    'D': {'B', 'C', 'F'},
-    'E': {'C', 'F', 'G'},
-    'F': {'D', 'E', 'G'},
-    'G': {'E', 'F'}
-}
+# Define the grid as a 2D list
+grid = [
+    ['S', '.', '.', '#', '.', '.', '.'],
+    ['.', '#', '.', '#', '.', '#', '.'],
+    ['.', '#', '.', '.', '.', '.', '.'],
+    ['.', '.', '#', '#', '.', '#', '.'],
+    ['.', '.', '#', '#', '.', '#', '.'],
+    ['.', '.', '#', '#', '.', '#', '.'],
+    ['.', '.', '#', '#', '.', '#', '.'],
+    ['.', '.', '#', '#', '.', '#', '.'],
+    ['.', '.', '#', '#', '.', '#', '.'],
+    ['#', '.', '#', '.', '.', '#', 'G']
+]
 
-start_node = 'A'
-goal_node = 'G'
+start = (0, 0)
+goal = (9, 6)
 
 # Find the shortest path using A* search
-path = astar_search(graph, start_node, goal_node)
+path = astar_search(grid, start, goal)
 
 print("Shortest path:", path)
